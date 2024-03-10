0ba197977dc2f33805e0355e35a5cdea4eed0ea9
DESKTOP-NPUVJIQ\Mark 1 <tabish169000@gmail.com>
2024-03-09 19:09:43 -0800

    created only markdown file

---
+++ src/app/(days)/day3/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure quibusdam expedita harum dolore saepe, blanditiis odit quaerat voluptates possimus accusantium, voluptas assumenda voluptatibus necessitatibus officiis! Adipisci optio recusandae incidunt cum?
+      </Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
